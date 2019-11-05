a2a83157c1364c8fd6b89060c72a7a5620935723
Kirsten <kmaaallen@gmail.com>
2019-11-05 19:22:07 +0000

    added cart app

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -20,11 +20,13 @@ from products import urls as urls_products
 from products.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT
+from cart import urls as urls_cart
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', all_products, name="index"),
     url(r'^accounts/', include(urls_accounts)),
     url(r'^products/', include(urls_products)),
+    url(r'^cart/', include(urls_cart)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
 ]
