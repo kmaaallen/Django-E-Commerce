5b8185316c55d20e14ccd8f82ffaa4c30c61d40e
Kirsten <kmaaallen@gmail.com>
2019-11-12 19:11:34 +0000

    made migrations , added default to county for db

--- checkout/forms.py
+++ checkout/forms.py
@@ -21,4 +21,4 @@ class OrderForm(forms.ModelForm):
         model = Order
         fields = ('full_name', 'phone_number', 'country', 'postcode',
                   'town_or_city', 'street_address1', 'street_address2',
-                  'county')
+                  'county',)
--- static/js/stripe.js
+++ static/js/stripe.js
@@ -1,17 +1,17 @@
 $(function(){
-    $('#payment_form').submit(function(){
+    $("#payment_form").submit(function(){
         var form = this;
         var card = {
             number: $("#id_credit_card_number").val(),
-            expMonth: $('#id_expiry_month').val(),
-            expYear: $('#id_expiry_year').val(),
-            cvc: $('#id_cvv').val(),
+            expMonth: $("#id_expiry_month").val(),
+            expYear: $("#id_expiry_year").val(),
+            cvc: $("#id_cvv").val(),
         };
 
     Stripe.createToken(card, function(status, response){
         if (status === 200){
             $("#credit-card-errors").hide();
-            $('#id_stripe_id').val(response.id);
+            $("#id_stripe_id").val(response.id);
 
             // Prevent the Credit card Details from being submitted to our server
             $("#id_credit_card_number").removeAttr('name');
